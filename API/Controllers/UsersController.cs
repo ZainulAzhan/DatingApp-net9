81c226f72e895ed2e48e8bf7d3095c45ed0827b5
Zainul Azhan <zainulazhan2014@gmail.com>
2025-02-01 23:02:22 +0800

    End of section 5

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,9 +6,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]
     public class UsersController(DataContext context) : BaseApiController
     {
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -16,6 +16,7 @@ namespace API.Controllers
             return users;
         }
 
+        [Authorize]
         [HttpGet("{id:int}")]  // /api/users/3
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
