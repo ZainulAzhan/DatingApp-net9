cd868551a199d13b4941e4efd2d5b1858da9b017
Zainul Azhan <zainulazhan2014@gmail.com>
2025-02-02 16:23:50 +0800

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,5 @@
-using System;
 using System.ComponentModel.DataAnnotations;
+using API.Extensions;
 
 namespace API.Entities;
 
@@ -10,9 +10,38 @@ public class AppUser
     [Required]
     public string UserName { get; set;}
 
-    [Required]
-    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordHash { get; set; } = [];
     
+    public byte[] PasswordSalt { get; set; } = [];
+
+    public DateOnly DateOfBirth { get; set; }
+
     [Required]
-    public byte[] PasswordSalt { get; set; }    
+    public string KnownAs { get; set; }
+
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    public string Gender { get; set; }
+
+    public string? Introduction { get; set; }
+
+    public string? Interests { get; set; }
+
+    public string? LookingFor { get; set; }
+
+    [Required]
+    public string City { get; set; }
+
+    [Required]
+    public string Country { get; set; }
+
+    public List<Photo> Photos { get; set; } = [];
+
+    // public int GetAge()
+    // {
+    //     return DateOfBirth.CalculateAge();
+    // }
 }
